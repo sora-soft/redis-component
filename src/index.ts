d61f11597dc367f69148bc967cd49611c6ee84bb
yaya <yaya@xyyaya.com>
2021-03-06 13:54:03 +0800

    feat: 更换 redis 底层库

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/RedisComponent';
 export * from './lib/RedisError';
 export * from './lib/RedisErrorCode';
+export * from 'handy-redis';
--- src/lib/RedisComponent.ts
+++ src/lib/RedisComponent.ts
@@ -1,7 +1,7 @@
 import {Component, IComponentOptions, Runtime} from '@sora-soft/framework';
-import { ClientV3 as RedisClient } from '@camaro/redis';
 import {RedisError} from './RedisError';
 import {RedisErrorCode} from './RedisErrorCode';
+import {createNodeRedisClient, WrappedNodeRedisClient} from 'handy-redis';
 
 export interface IRedisComponentOptions extends IComponentOptions {
   host: string;
@@ -9,6 +9,7 @@ export interface IRedisComponentOptions extends IComponentOptions {
   db: number;
   username?: string;
   password?: string;
+  prefix: string;
 }
 
 class RedisComponent extends Component {
@@ -17,13 +18,12 @@ class RedisComponent extends Component {
   }
 
   protected async connect() {
-    this.client_ = new RedisClient(this.redisOptions_);
-    await this.client_.SELECT(this.redisOptions_.db);
+    this.client_ = createNodeRedisClient(this.redisOptions_);
     Runtime.frameLogger.success('component.redis', { event: 'connect', target: {host: this.redisOptions_.host, port: this.redisOptions_.port, db: this.redisOptions_.db} });
   }
 
   protected async disconnect() {
-    await this.client_.QUIT();
+    await this.client_.quit();
     this.client_ = null;
     Runtime.frameLogger.success('component.redis', { event: 'disconnect', target: {host: this.redisOptions_.host, port: this.redisOptions_.port, db: this.redisOptions_.db} });
   }
@@ -34,8 +34,19 @@ class RedisComponent extends Component {
     return this.client_;
   }
 
+  async setJSON(key: string, object: Object, ttlMs?: number) {
+    await this.client_.set(key, JSON.stringify(object), ['PX', ttlMs]);
+  }
+
+  async getJSON(key: string) {
+    const content = await this.client_.get(key);
+    if (content)
+      return JSON.parse(content);
+    return null;
+  }
+
   private redisOptions_: IRedisComponentOptions;
-  private client_: RedisClient;
+  private client_: WrappedNodeRedisClient;
 }
 
 export {RedisComponent}
